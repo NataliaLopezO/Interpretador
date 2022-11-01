ef4e66cb39f1fce7168c7af3b79ee0dd7e6fd81f
carolain3472 <carolain.jimenez@correounivalle.edu.co>
2022-11-01 11:02:23 -0500

    Codigos del grupo

--- Taller3.rkt
+++ Taller3.rkt
@@ -1,7 +1,7 @@
 #lang eopl
 ;;INTERPRETADOR
-;; Natalia Lopez Osorio
-;;Carolain JImenez Bedoya
-;;Juan Steban Diaz
-;;Gabriel Franco
-;;Hernando Lopez
+;; Natalia Lopez Osorio - 2025618
+;;Carolain JImenez Bedoya - 2071368
+;;Juan Steban Diaz - 2024147
+;;Gabriel Franco - 2024200
+;;Hernando Lopez - 2022318
