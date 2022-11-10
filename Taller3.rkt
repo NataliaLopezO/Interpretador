145eed8bc715b035d4b4fcbb8333fa2492c10d01
DiazSSJ <juans220304@gmail.com>
2022-11-09 23:44:12 -0500

    link del git

--- Taller3.rkt
+++ Taller3.rkt
@@ -1,10 +1,12 @@
 #lang eopl
 ;;INTERPRETADOR
 ;; Natalia Lopez Osorio - 2025618
-;;Carolain JImenez Bedoya - 2071368
+;;Carolain Jimenez Bedoya - 2071368
 ;;Juan Steban Diaz - 2024147
 ;;Gabriel Franco - 2024200
 ;;Hernando Lopez - 2022318
+
+;;Repositorio en GitHub: https://github.com/NataliaLopezO/Interpretador.git
 
 ;******************************************************************************************
 
